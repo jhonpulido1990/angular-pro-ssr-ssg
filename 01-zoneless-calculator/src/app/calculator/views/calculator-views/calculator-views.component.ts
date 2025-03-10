288d00b50f196ddf4aa00f83dc270e6e3a492fa1
jhonpulido1990 <3825@holbertonstudents.com>
2025-03-10 15:22:10 -0500

    diseño y esctructura

---
+++ 01-zoneless-calculator/src/app/calculator/views/calculator-views/calculator-views.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-views',
+  imports: [],
+  templateUrl: './calculator-views.component.html',
+})
+export default class CalculatorViewsComponent { }
