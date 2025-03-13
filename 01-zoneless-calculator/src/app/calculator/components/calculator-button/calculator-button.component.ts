5886437831caf5625fbd5066855253919c0847e6
jhonpulido1990 <3825@holbertonstudents.com>
2025-03-13 16:44:47 -0500

    inputsignal y hosbinding viewEncapsulation

--- 01-zoneless-calculator/src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ 01-zoneless-calculator/src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,12 +1,31 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-calculator-button',
   imports: [],
   templateUrl: './calculator-button.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl: './calculator-button.component.css',
   host: {
-    class: "w-1/4 border-r border-b border-indigo-400"
-  }
+    class: 'w-1/4 border-r border-b border-indigo-400',
+  },
 })
-export class CalculatorButtonComponent { }
+export class CalculatorButtonComponent {
+  public isCommand = input(false, {
+    transform: (value: boolean | string) =>
+      typeof value === 'string' ? value === '' : value,
+  });
+
+  public isDoubleSize = input(false, {
+    transform: (value: boolean | string) =>
+      typeof value === 'string' ? value === '' : value,
+  });
+
+  //@HostBinding('class.is-command') get commandStyle() {
+  //  return this.isCommand();
+  //}
+
+  @HostBinding('class.w-2/4') get commandStyle() {
+    return this.isDoubleSize();
+  }
+}
